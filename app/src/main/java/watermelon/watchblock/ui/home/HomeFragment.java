d4cc7433bbf27d192f5f06a27d0f7d6c3c83c5ce
John Ng <jng15@cs.unm.edu>
2019-10-05 13:26:59 -0600

    checkup

--- app/src/main/java/watermelon/watchblock/ui/home/HomeFragment.java
+++ app/src/main/java/watermelon/watchblock/ui/home/HomeFragment.java
@@ -1,17 +1,28 @@
 package watermelon.watchblock.ui.home;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.location.LocationListener;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -24,8 +35,32 @@ import watermelon.watchblock.Submission;
 
 public class HomeFragment extends Fragment
 {
-
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected Context context;
+    TextView txtLat;
+    String lat;
+    String provider;
+    protected String latitude,longitude;
+    protected boolean gps_enabled,network_enabled;
     private HomeViewModel homeViewModel;
+
+
+    private void getLocation()
+    {
+//        if (ActivityCompat.checkSelfPermission(mActivity, Manifest.permission.ACCESS_FINE_LOCATION) !=
+//                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mActivity, Manifest.permission.ACCESS_COARSE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED)
+//        {
+//
+//            ActivityCompat.requestPermissions(mActivity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+//        }
+//        else
+//        {
+//            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+//            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//        }
+    }
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -52,6 +87,21 @@ public class HomeFragment extends Fragment
             public void onClick(View view) {
                 // upload file
                 System.out.println("FILE");
+                String time = "TIME";
+                Date currentTime = Calendar.getInstance().getTime();
+                time = currentTime.toString();
+                int duration = 2000;
+
+
+
+                Snackbar mySnackbar = Snackbar.make(view, time, duration);
+                mySnackbar.show();
+
+
+
+//                locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+//                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+//                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
             }
         });
 
