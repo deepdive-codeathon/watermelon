ac320d2234429f9aa7c4eaddaebe2049719d8b31
John Ng <jng15@cs.unm.edu>
2019-10-05 10:35:22 -0600

    Added smash me mario

--- app/src/main/java/watermelon/watchblock/ui/home/HomeFragment.java
+++ app/src/main/java/watermelon/watchblock/ui/home/HomeFragment.java
@@ -1,22 +1,32 @@
 package watermelon.watchblock.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 import watermelon.watchblock.R;
+import watermelon.watchblock.Submission;
 
 public class HomeFragment extends Fragment
 {
 
     private HomeViewModel homeViewModel;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState)
@@ -24,6 +34,18 @@ public class HomeFragment extends Fragment
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        //Start button that initiates the game.
+        Button buttonSolo = root.findViewById(R.id.submitTipButton);
+        buttonSolo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Submission BDSM = new Submission();
+            }
+        });
+
+
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>()
         {
