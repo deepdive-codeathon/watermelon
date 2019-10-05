16b1e6792a063f068228c930639a29d728af4bdf
Mario Morford-Oberst <morfordoberst@cs.unm.edu>
2019-10-05 13:36:42 -0600

    creates audio stream for phone call, but not yet linked to calling

--- app/src/main/java/watermelon/watchblock/Submission.java
+++ app/src/main/java/watermelon/watchblock/Submission.java
@@ -1,9 +1,18 @@
 package watermelon.watchblock;
+import android.app.Application;
+import android.speech.tts.TextToSpeech;
 
 public class Submission
 {
+    //TextToSpeech tts = new TextToSpeech(super.getClass().)
+
     public Submission()
     {
         System.out.println("OH MARIOOOOOOOOO");
+
     }
+
+
+
+
 }
--- app/src/main/java/watermelon/watchblock/ui/home/HomeFragment.java
+++ app/src/main/java/watermelon/watchblock/ui/home/HomeFragment.java
@@ -1,24 +1,23 @@
 package watermelon.watchblock.ui.home;
 
-import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.speech.tts.TextToSpeech;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+
+import java.util.Locale;
+
 import watermelon.watchblock.R;
 import watermelon.watchblock.Submission;
 
@@ -27,7 +26,28 @@ public class HomeFragment extends Fragment
 
     private HomeViewModel homeViewModel;
 
+    TextToSpeech tts;
 
+    private void speak(){
+        tts = new TextToSpeech(getActivity(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+
+                String text = "Juniper hates Kelsie";
+                tts.setLanguage(Locale.US);
+                if (status == TextToSpeech.SUCCESS){
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                        String utteranceId = this.hashCode() + "";
+                        Bundle params = new Bundle();
+                        params.putInt(TextToSpeech.Engine.KEY_PARAM_STREAM, AudioManager.STREAM_VOICE_CALL);
+                        tts.speak(text, TextToSpeech.QUEUE_FLUSH, params, utteranceId);
+                        System.out.println("I have spoken");
+                    }
+                }
+
+            }
+        });
+    }
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState)
     {
@@ -38,10 +58,17 @@ public class HomeFragment extends Fragment
 
         //Start button that initiates the game.
         Button buttonSolo = root.findViewById(R.id.submitTipButton);
+
+
+
+
+
         buttonSolo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Submission BDSM = new Submission();
+                speak();
+
             }
         });
 
