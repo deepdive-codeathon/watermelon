68b0ed648771f005b2ab2001e8b05716ead2ab22
Alan C Shen <alshen@cs.unm.edu>
2019-10-05 18:44:42 -0600

    Blockchain and maps

--- app/src/main/java/watermelon/watchblock/ui/crimemap/DashboardFragment.java
+++ app/src/main/java/watermelon/watchblock/ui/crimemap/DashboardFragment.java
@@ -5,20 +5,35 @@ import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-import watermelon.watchblock.Crime;
 import watermelon.watchblock.R;
 
-public class DashboardFragment extends Fragment
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import static watermelon.watchblock.MainActivity.uuid;
+
+public class DashboardFragment extends Fragment implements OnMapReadyCallback
 {
 
     private DashboardViewModel dashboardViewModel;
+    String update;
+    private GoogleMap mMap;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState)
@@ -29,18 +44,85 @@ public class DashboardFragment extends Fragment
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>()
-        {
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onChanged(@Nullable String s)
-            {
-                textView.setText(s);
+            public void onMapReady(GoogleMap mMap) {
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+                mMap.clear(); //clear old markers
+
+                CameraPosition googlePlex = CameraPosition.builder()
+                        .target(new LatLng(35.1032075,-106.6011941))
+                        .zoom(10)
+                        .bearing(0)
+                        .tilt(45)
+                        .build();
+//
+
+
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 1000, null);
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(35.1032075, -106.6011941))
+                        .title("Spider Man"));
+
             }
         });
-        TextView crime_list =root.findViewById(R.id.crime_list);
-        Crime crime1 = new Crime("Kelsie");
-        crime_list.setText(crime_list.getText() + crime1.toString());
+
+        try
+        {
+            update();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
         return root;
     }
+    private void update() throws Exception {
+
+        String url = "https://test.devv.io/update";
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Content-Type", "application/json; utf-8");
+        con.setRequestProperty("Accept", "application/json");
+
+        String urlParameters = "{ \"uuid\": \"" + uuid + "\"}";
+        con.setDoOutput(true);
+
+        try(OutputStream os = con.getOutputStream()) {
+            byte[] input = urlParameters.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        System.out.println(con.getResponseCode());
+        System.out.println(con.getResponseMessage());
+
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            System.out.println(response.toString());
+            update = response.toString();
+        }
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(4, 5);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
 }
