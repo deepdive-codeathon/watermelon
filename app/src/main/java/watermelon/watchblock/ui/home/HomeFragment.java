3ec1ae242100d40899138c4237af7a518de2e012
Mario Morford-Oberst <morfordoberst@cs.unm.edu>
2019-10-05 18:40:57 -0600

    add beta version of calling, commented out.  when used, it crashes the program

--- app/src/main/java/watermelon/watchblock/ui/home/HomeFragment.java
+++ app/src/main/java/watermelon/watchblock/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package watermelon.watchblock.ui.home;
 
+
 import android.media.AudioManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -9,6 +10,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.speech.tts.TextToSpeech;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View.OnClickListener;
+import android.content.Context;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -33,13 +44,16 @@ public class HomeFragment extends Fragment
             @Override
             public void onInit(int status) {
 
-                String text = "Juniper hates Kelsie";
+                String text = "Hello 9-1-1, this is Juniper calling.  Kelsie shot me this morning and put me in her phone.  Please help me I am in serious danger.";
+       //         String text = "ni hao 9-1-1. wo shi juniper, kelsie hen bu hao ta shi yi ge ne ge hei gui.";
+                String text2 = "Hello, I am block chain.  Let me block chain you and win.";
+                String text3 = "Uh oh, didn't make toilet!";
                 tts.setLanguage(Locale.US);
                 if (status == TextToSpeech.SUCCESS){
                     if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
                         String utteranceId = this.hashCode() + "";
                         Bundle params = new Bundle();
-                        params.putInt(TextToSpeech.Engine.KEY_PARAM_STREAM, AudioManager.STREAM_VOICE_CALL);
+                        params.putInt(TextToSpeech.Engine.KEY_PARAM_STREAM, AudioManager.STREAM_SYSTEM);
                         tts.speak(text, TextToSpeech.QUEUE_FLUSH, params, utteranceId);
                         System.out.println("I have spoken");
                     }
@@ -48,6 +62,17 @@ public class HomeFragment extends Fragment
             }
         });
     }
+
+    private void makeCall(){
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:15055500216"));
+        startActivity(callIntent);
+    }
+
+
+
+
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState)
     {
@@ -61,6 +86,10 @@ public class HomeFragment extends Fragment
 
 
 
+        //create PhoneStateListener
+        PhoneCallListener phoneListener = new PhoneCallListener();
+        TelephonyManager telephonyManager = (TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+        telephonyManager.listen(phoneListener,PhoneStateListener.LISTEN_CALL_STATE);
 
 
         buttonSolo.setOnClickListener(new View.OnClickListener() {
@@ -68,6 +97,7 @@ public class HomeFragment extends Fragment
             public void onClick(View view) {
                 Submission BDSM = new Submission();
                 speak();
+                //makeCall();
 
             }
         });
@@ -83,5 +113,50 @@ public class HomeFragment extends Fragment
             }
         });
         return root;
+    }
+
+    //monitor phone call activities
+    private class PhoneCallListener extends PhoneStateListener {
+
+        private boolean isPhoneCalling = false;
+
+        String LOG_TAG = "LOGGING 123";
+
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+
+            if (TelephonyManager.CALL_STATE_RINGING == state) {
+                // phone ringing
+                Log.i(LOG_TAG, "RINGING, number: " + incomingNumber);
+            }
+
+            if (TelephonyManager.CALL_STATE_OFFHOOK == state) {
+                // active
+                Log.i(LOG_TAG, "OFFHOOK");
+
+                isPhoneCalling = true;
+            }
+
+            if (TelephonyManager.CALL_STATE_IDLE == state) {
+                // run when class initial and phone call ended,
+                // need detect flag from CALL_STATE_OFFHOOK
+                Log.i(LOG_TAG, "IDLE");
+
+                if (isPhoneCalling) {
+
+                    Log.i(LOG_TAG, "restart app");
+
+                    // restart app
+                    Intent i = getActivity().getApplicationContext().getPackageManager()
+                            .getLaunchIntentForPackage(
+                                    getActivity().getApplicationContext().getPackageName());
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+
+                    isPhoneCalling = false;
+                }
+
+            }
+        }
     }
 }
